2ae5a788edd7a0114748ae6ab61b57e1ddbc0424
sabmat <100289478+sabmat@users.noreply.github.com>
2022-09-18 21:26:47 +0300

    finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const imageCards = addImgGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", imageCards);
+galleryContainer.addEventListener("click", onGalleryContainer);
+
+
+
+function addImgGallery(galleryItems) {
+    return galleryItems
+      .map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+      </a>`
+      })
+      .join("");
+  }
+    
+function onGalleryContainer (evt) {
+    evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  
+ const  gallery = new SimpleLightbox('.gallery a');
+  
+  gallery.on("show.simplelightbox", function () {
+    gallery.defaultOptions.captionDelay = 250;
+  })}
